1495add1ffdaed6da42584e7cfbc460447991873
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-03-13 14:40:20 +0900

    added dataset, token_dict

---
+++ sobamchan_dataset.py
@@ -0,0 +1,31 @@
+import xml.etree.ElementTree as ET
+
+class AGCorpus:
+
+    def __init__(self, datapath):
+        self.tree = ET.parse(datapath)
+        self.titles = None
+        self.categories = None
+        self.descs = None
+
+    def get_data(self):
+        titles = [ title_ele.text for title_ele in self.tree.findall('title') ]
+        categories = [ category_ele.text for category_ele in self.tree.findall('category') ]
+        descs = [ desc_ele.text for desc_ele in self.tree.findall('description') ]
+
+        self.titles = titles
+        self.categories = categories
+        self.descs = descs
+
+        classes = {'Business': 0, 'Entertainment': 1, 'World': 2, 'Sports': 3}
+        X = []
+        T = []
+        for category, desc in zip(categories, descs):
+            if category in classes.keys() and desc is not None:
+                X.append(desc)
+                T.append(classes[category])
+
+        return T, X
+
+if __name__ == '__main__':
+    ag = AGCorpus('/Users/sochan/project/NLP/datas/newsspace200.xml')
