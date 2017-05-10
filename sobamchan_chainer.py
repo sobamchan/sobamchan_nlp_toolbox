4a82629be459808656e43b8ff93ed439494d52a8
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-05-10 15:01:08 +0900

    added gpu convert function

--- sobamchan_chainer.py
+++ sobamchan_chainer.py
@@ -1,5 +1,6 @@
 import numpy as np
 import chainer
+from chainer import cuda
 from chainer import serializers
 from chainer import Chain
 import os
@@ -9,6 +10,9 @@ class Model(Chain):
 
     def __init__(self, **kwargs):
         super(Model, self).__init__(**kwargs)
+        
+        self.gpu = -1
+        self.xp = np
 
     def save_model(self, filename='./model.model', data_format=None):
         path, ext = os.path.splitext(filename)
@@ -27,3 +31,20 @@ class Model(Chain):
 
 
         return chainer.Variable(x, volatile=volatile)
+
+    def check_gpu(self, gpu):
+        self.gpu = gpu
+        if gpu >= 0:
+            cuda.get_device(gpu).use()
+            self.to_gpu()
+            self.xp = chainer.cuda.cupy
+            return self.xp
+        self.xp = np
+        return np
+
+    def get_xp(self):
+        gpu = self.gpu
+        if gpu < 0:
+            return self.xp
+        else:
+            return self.xp
