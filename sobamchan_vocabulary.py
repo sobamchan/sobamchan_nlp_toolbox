4f55e73285dbab7c6e346efe4affbc2d407f9607
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2017-07-28 19:43:04 +0900

    update vocabulary for unknown words

--- sobamchan_vocabulary.py
+++ sobamchan_vocabulary.py
@@ -1,6 +1,7 @@
 import collections
 import pickle
 
+
 class Vocabulary(object):
 
     def __init__(self):
@@ -8,6 +9,7 @@ class Vocabulary(object):
         self.i2w = {}
         self.counts = collections.Counter()
         self.dataset = []
+        self.replaced = False
 
     def __len__(self):
         return len(self.w2i)
@@ -19,6 +21,8 @@ class Vocabulary(object):
         returns:
             None
         '''
+        self.w2i['<UNK>'] = 0
+        self.i2w[0] = '<UNK>'
         for word in line.strip().split():
             word = word.lower()
             if word not in self.w2i:
@@ -28,12 +32,36 @@ class Vocabulary(object):
             self.counts[self.w2i[word]] += 1
             self.dataset.append(self.w2i[word])
 
+    def replace_rare_unk(self, min_occ_n):
+        new_counter = collections.Counter()
+        new_w2i = {}
+        new_i2w = {}
+        # add <UNK>
+        new_w2i['<UNK>'] = 0
+        new_i2w[0] = '<UNK>'
+        for k, v in self.counts.items():
+            if v > min_occ_n:
+                word = self.i2w[k]
+                new_ind = len(new_w2i)
+                new_w2i[word] = new_ind
+                new_i2w[new_ind] = word
+                new_counter[new_ind] = self.counts[k]
+            else:
+                new_counter[0] += self.counts[k]
+        self.counts = new_counter
+        self.i2w = new_i2w
+        self.w2i = new_w2i
+        self.replaced = True
+
     def encode(self, line):
         w2i = self.w2i
         encoded_line = []
         for word in line.strip().split():
             word = word.lower()
-            encoded_line.append(w2i[word])
+            if word in self.w2i.keys():
+                encoded_line.append(w2i[word])
+            else:
+                encoded_line.append(w2i['<UNK>'])
 
         return encoded_line
 
